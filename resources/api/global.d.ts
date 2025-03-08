7740f19a3ebc84e5f734d175fbc9bcb8812d0c91
Daniel Molina <damolina@outlook.com>
2025-03-08 15:18:54 -0800

    Update jsconfig.json (known issue: all files are assumed to be part of the same context).
    Update global.d.ts for better Intellisense

--- resources/api/global.d.ts
+++ resources/api/global.d.ts
@@ -6,24 +6,24 @@ declare global {
    * Visual Studio Code Extension API.
    * See https://code.visualstudio.com/api for more information.
    */
-  declare const vscode: typeof _vscode;
+  const vscode: typeof _vscode;
 
   /**
    * A [CancellationToken](https://code.visualstudio.com/api/references/vscode-api#CancellationToken)
    * used by the extension to notify about a stop request. 
    */
-  declare const __cancellationToken: _vscode.CancellationToken;
+  const __cancellationToken: _vscode.CancellationToken;
 
   /**
    * Id of the current macro execution session.
    */
-  declare const __runId: string;
+  const __runId: string;
 
   /**
    * Macros Extension for Visual Studio Code API.
    * See https://github.com/damolinx/vscode-macros#readme for more information.
    */
-  declare const macros: {
+  const macros: {
     /**
      * Current macro.
      */
@@ -34,4 +34,6 @@ declare global {
       readonly uri: _vscode.Uri | undefined;
     }
   };
-};
+}
+
+export { };
