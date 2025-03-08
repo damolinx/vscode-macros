63088ecb7405c3c2fd764dec276145337611148f
Daniel Molina <damolina@outlook.com>
2025-03-08 15:10:30 -0800

    Update cancellation template.

--- resources/examples/cancellation.js
+++ resources/examples/cancellation.js
@@ -1,21 +1,15 @@
-
 async function waitForCancellation() {
   return vscode.window.withProgress(
     {
-      title: `Cancel '${__runId}' from 'Show Running Macros' command …`,
+      title: `Try cancelling '${__runId}' using the 'Show Running Macros' command …`,
       location: vscode.ProgressLocation.Notification,
       cancellable: true
     },
-    (progress, token) => {
-      return new Promise((resolve, _reject) => {
-        token.onCancellationRequested(() => {
-          resolve('Cancelled from notification dialog');
-        });
-        __cancellationToken.onCancellationRequested(() => {
-          resolve('Cancelled from extension');
-        });
-      });
-    });
+    (progress, token) => new Promise((resolve) => {
+      token.onCancellationRequested(() => resolve('Cancelled from notification dialog'));
+      __cancellationToken.onCancellationRequested(() => resolve('Cancelled from extension'));
+    })
+  );
 }
 
 waitForCancellation().then((result) => vscode.window.showInformationMessage(result));
