7257ddffca192d58d5c14f192c84e93b9162c7a6
Daniel Molina <damolina@outlook.com>
2025-05-22 21:32:30 -0700

    Update

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const MACRO_LANGUAGE = 'javascript';
-export const MACRO_LANGUAGES = [MACRO_LANGUAGE];
+export const MACRO_LANGUAGES: readonly string[] = [MACRO_LANGUAGE];
 
 export const MACRO_EXTENSION = '.macro.js';
-export const MACRO_EXTENSIONS = [MACRO_EXTENSION, '.js', '.cjs'];
+export const MACRO_EXTENSIONS: readonly string[] = [MACRO_EXTENSION, '.js', '.cjs'];
 
-export const MACROS_FILTER = {
+export const MACROS_FILTER: Record<string, Array<string>> = {
   'JavaScript Macro': ['macro.js'],
   'JavaScript': ['js', 'cjs'],
 };
