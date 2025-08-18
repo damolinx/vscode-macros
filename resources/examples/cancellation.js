e1f04454fa908d7cfdbf0285a2bc2ee820cbe81a
Daniel Molina <damolina@outlook.com>
2025-08-18 09:16:07 -0700

    Fix Template

--- resources/examples/cancellation.js
+++ resources/examples/cancellation.js
@@ -7,11 +7,11 @@ async function main() {
   },
     (progress, token) => new Promise((resolve) => {
       token.onCancellationRequested(() => {
-        progress.message = 'Canceled from notification';
+        progress.report({ message: 'Canceled from notification' });
         resolve();
       });
       __cancellationToken.onCancellationRequested(() => {
-        progress.message = 'Canceled from \'Show Running Macros\'';
+        progress.report({ message: 'Canceled from \'Show Running Macros\'' });
         resolve();
       });
     }));
