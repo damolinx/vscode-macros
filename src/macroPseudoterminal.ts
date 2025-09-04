549b60586649dc7b2daaf46c6206644f7ed8ceb1
Daniel Molina <damolina@outlook.com>
2025-09-03 19:37:59 -0700

    Update text

--- src/macroPseudoterminal.ts
+++ src/macroPseudoterminal.ts
@@ -79,7 +79,7 @@ export class MacroPseudoterminal implements vscode.Pseudoterminal {
     }
 
     this.onDidWriteEmitter.fire(
-      `\x1b[1mMacros REPL\x1b[0m — JS/TS execution with full extension context and API access.${REPL_NEWLINE}Type ".help" for more information.${REPL_NEWLINE}${REPL_NEWLINE}`,
+      `\x1b[1mMacros REPL\x1b[0m — JS/TS execution with extension context and API access.${REPL_NEWLINE}Type ".help" for more information.${REPL_NEWLINE}${REPL_NEWLINE}`,
     );
 
     const input = new PassThrough();
