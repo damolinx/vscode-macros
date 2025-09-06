8f8a4438a5d561859dfea997f34aa7eaa303871e
Daniel Molina <damolina@outlook.com>
2025-09-06 16:14:00 -0700

    Remove unused file.

--- src/commands/openMacroLibrary.ts
+++
@@ -1,7 +0,0 @@
-// import { MacroLibrary } from '../core/library/macroLibrary';
-// import { ExtensionContext } from '../extensionContext';
-// import { showMacroQuickPick } from '../ui/dialogs';
-// import { toUri } from '../utils/uri';
-// import { showTextDocument } from '../utils/vscodeEx';
-
-// export async function openMacroLibrary(library: MacroLibrary) {}
