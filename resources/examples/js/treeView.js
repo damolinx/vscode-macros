df37cff5cd519accc4f509ee3e83861b0ec9f2f4
Daniel Molina <damolina@outlook.com>
2025-11-03 19:12:42 -0800

    Update for symmetry

--- resources/examples/js/treeView.js
+++ resources/examples/js/treeView.js
@@ -35,6 +35,7 @@ function createTreeProvider() {
   };
 }
 
+/** @returns {import('vscode').TreeView<string>} */
 function createTreeView() {
   const treeView = vscode.window.createTreeView(
     viewId, {
