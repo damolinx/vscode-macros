4284a7b6b46247405673bcf4657b546c9423baaf
Daniel Molina <damolina@outlook.com>
2025-11-30 17:43:40 -0800

    Clean-up API

--- src/core/macroId.ts
+++ src/core/macroId.ts
@@ -3,6 +3,6 @@ import * as vscode from 'vscode';
 export type MacroId = string & { __macroIdBrand: void };
 
 export function getMacroId(uri: vscode.Uri): MacroId {
-  const normalizedUri = uri.scheme === 'file' ? uri : uri.with({ fragment: '', query: '' });
+  const normalizedUri = uri.with({ fragment: '', query: '' });
   return normalizedUri.toString(true) as MacroId;
 }
