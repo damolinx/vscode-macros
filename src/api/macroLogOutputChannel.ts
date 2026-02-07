6e66b4372eb3c3d83ee08f4e33db91e14cb8a92f
Daniel Molina <damolina@outlook.com>
2026-02-07 14:05:30 -0800

    Clean-up API

--- src/api/macroLogOutputChannel.ts
+++ src/api/macroLogOutputChannel.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { inspect } from 'util';
 import { SandboxExecutionId } from '../core/execution/sandboxExecutionId';
 import { ExtensionContext } from '../extensionContext';
 import { cleanStack } from '../utils/errors';
@@ -8,20 +9,70 @@ import { cleanStack } from '../utils/errors';
  * the macro run ID and prevent execution of unsafe operations.
  */
 export class MacroLogOutputChannel implements vscode.LogOutputChannel {
-  readonly #id: string;
+  readonly #executionId: string;
   readonly #log: vscode.LogOutputChannel;
+  readonly #prefix: string;
 
   constructor(executionId: SandboxExecutionId, { log }: ExtensionContext) {
+    this.#executionId = executionId;
     this.#log = log;
-    this.#id = `[${executionId}]`;
+    this.#prefix = `[${executionId}]`;
   }
 
-  #blocked(op: string): void {
-    this.warn(`Macro is not allowed to call \`${op}\`; ignoring call.`);
+  dispose(): void {
+    this.#blocked('dispose');
   }
 
-  #prefix(message: string): string {
-    return `${this.#id} ${message}`;
+  [inspect.custom]() {
+    return {
+      type: 'LogOutputChannel',
+      logLevel: vscode.LogLevel[this.logLevel],
+      runId: this.#executionId,
+    };
+  }
+
+  #blocked(methodName: string): void {
+    this.warn(`Macros cannot call \`${methodName}\`; ignoring.`);
+  }
+
+  #withPrefix(message: string): string {
+    return message ? `${this.#prefix} ${message}` : this.#prefix;
+  }
+
+  append(value: string): void {
+    if (value) {
+      this.#log.append(this.#withPrefix(value));
+    }
+  }
+
+  appendLine(value: string): void {
+    this.#log.appendLine(this.#withPrefix(value));
+  }
+
+  clear(): void {
+    this.#blocked('clear');
+  }
+
+  debug(message: string, ...args: any[]): void {
+    this.#log.debug(this.#withPrefix(message), ...args);
+  }
+
+  error(error: string | Error, ...args: any[]): void {
+    if (typeof error === 'string') {
+      return this.#log.error(this.#withPrefix(error), ...args);
+    }
+
+    const withIdError = new Error(this.#withPrefix(String(error)), { cause: error });
+    withIdError.stack = error.stack && cleanStack(error.stack);
+    this.#log.error(withIdError, ...args);
+  }
+
+  info(message: string, ...args: any[]): void {
+    this.#log.info(this.#withPrefix(message), ...args);
+  }
+
+  hide(): void {
+    this.#log.hide();
   }
 
   get logLevel(): vscode.LogLevel {
@@ -36,44 +87,6 @@ export class MacroLogOutputChannel implements vscode.LogOutputChannel {
     return this.#log.onDidChangeLogLevel;
   }
 
-  append(value: string): void {
-    this.#log.append(value);
-  }
-
-  appendLine(value: string): void {
-    this.#log.appendLine(value);
-  }
-
-  clear(): void {
-    this.#blocked('clear');
-  }
-
-  debug(message: string, ...args: any[]): void {
-    this.#log.debug(this.#prefix(message), ...args);
-  }
-
-  dispose(): void {
-    this.#blocked('dispose');
-  }
-
-  error(error: string | Error, ...args: any[]): void {
-    if (typeof error === 'string') {
-      return this.#log.error(this.#prefix(error), ...args);
-    }
-
-    const withIdError = new Error(this.#prefix(error.message), { cause: error });
-    withIdError.stack = error.stack && cleanStack(error.stack);
-    this.#log.error(withIdError, ...args);
-  }
-
-  hide(): void {
-    this.#log.hide();
-  }
-
-  info(message: string, ...args: any[]): void {
-    this.#log.info(this.#prefix(message), ...args);
-  }
-
   replace(_value: string): void {
     this.#blocked('replace');
   }
@@ -85,10 +98,10 @@ export class MacroLogOutputChannel implements vscode.LogOutputChannel {
   }
 
   trace(message: string, ...args: any[]): void {
-    this.#log.trace(this.#prefix(message), ...args);
+    this.#log.trace(this.#withPrefix(message), ...args);
   }
 
   warn(message: string, ...args: any[]): void {
-    this.#log.warn(this.#prefix(message), ...args);
+    this.#log.warn(this.#withPrefix(message), ...args);
   }
 }
