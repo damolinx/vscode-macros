914874208f50ee233a2f65e62f49e1079a7c9535
Daniel Molina <damolina@outlook.com>
2026-03-07 20:41:25 -0800

    Clean-up renderers on load.

--- src/api/window/renderers.ts
+++ src/api/window/renderers.ts
@@ -1,3 +1,4 @@
+import * as ts from 'typescript';
 import { ExtensionContext } from '../../extensionContext';
 import { readFile } from '../../utils/resources';
 import { Kind } from './node';
@@ -8,10 +9,23 @@ export const SUPPORTED_RENDERERS: readonly Kind[] = ['button', 'container', 'inp
 
 export const RendererScripts = new Map<Kind, Script>();
 
-export async function loadRenderers({ extensionContext }: ExtensionContext): Promise<void> {
-  for (const kind of SUPPORTED_RENDERERS) {
-    const path = `${MACRO_RENDERERS_DIR_RESOURCE}/${kind}.js`;
-    const code = await readFile(extensionContext, path);
-    RendererScripts.set(kind, new Script(code));
-  }
+export async function loadRenderers({ extensionContext, log }: ExtensionContext): Promise<void> {
+  const printer = ts.createPrinter({ removeComments: true });
+  const start = Date.now();
+  await Promise.all(
+    SUPPORTED_RENDERERS.map(async (kind) => {
+      const path = `${MACRO_RENDERERS_DIR_RESOURCE}/${kind}.js`;
+      const code = await readFile(extensionContext, path);
+      const source = ts.createSourceFile(
+        path,
+        code,
+        ts.ScriptTarget.Latest,
+        false,
+        ts.ScriptKind.JS,
+      );
+      const minifiedCode = printer.printFile(source);
+      RendererScripts.set(kind, new Script(minifiedCode));
+    }),
+  );
+  log.debug(`Loaded UI component renderers in ${Date.now() - start}ms`);
 }
