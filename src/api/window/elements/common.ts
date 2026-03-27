7827d5ec80f77d02a02a46ec469e1668b1dfe9a7
Daniel Molina <damolina@outlook.com>
2026-03-26 20:29:43 -0700

    Fix: DSL call with no options or children fails

--- src/api/window/elements/common.ts
+++ src/api/window/elements/common.ts
@@ -4,20 +4,20 @@ import { ElementNode, ElementNodeOptions } from './elementNode';
 export function createNodeFactory<TOptions extends ElementNodeOptions, TNode extends ElementNode>(
   constructor: new (options: TOptions | undefined, children: Node[]) => TNode,
 ) {
-  return function createNode(first: Node | TOptions, ...rest: Node[]): TNode {
+  return function createNode(first: Node | TOptions | undefined, ...rest: Node[]): TNode {
     const { options, children } = normalizeOptionsAndChildren<TOptions>(first, rest);
     return new constructor(options, children);
   };
 }
 
-export function normalizeOptionsAndChildren<TOptions extends ElementNodeOptions>(
-  childOrOptions: Node | TOptions,
+function normalizeOptionsAndChildren<TOptions extends ElementNodeOptions>(
+  childOrOptions: Node | TOptions | undefined,
   restChildren: Node[],
 ): { children: Node[]; options: TOptions | undefined } {
   let children: Node[];
   let options: TOptions | undefined;
 
-  if ('kind' in childOrOptions) {
+  if (childOrOptions && 'kind' in childOrOptions) {
     children = [childOrOptions, ...restChildren];
   } else {
     options = childOrOptions;
