7ad7ba55238fa589cb892d1ecb827eaadae9162e
Daniel Molina <damolina@outlook.com>
2026-03-28 21:17:16 -0700

    Update logging. Fix ID issue.

--- src/commands/stopMacro.ts
+++ src/commands/stopMacro.ts
@@ -1,24 +1,35 @@
 import * as vscode from 'vscode';
 import { SandboxExecution } from '../core/execution/sandboxExecution';
 import { Macro } from '../core/macro';
-import { getMacroId } from '../core/macroId';
 import { StartupMacro } from '../core/startupMacro';
 import { ExtensionContext } from '../extensionContext';
 import { resolveUri } from '../utils/uri';
+import { getMacroUriFromStartupMacroUri } from '../core/startupMacroId';
+import { SandboxExecutionId } from '../core/execution/sandboxExecutionId';
+import { formatDisplayUri } from '../utils/ui';
 
 export async function stopMacro(
   { log, sandboxManager }: ExtensionContext,
   target: Macro | SandboxExecution | StartupMacro | vscode.Uri,
 ): Promise<void> {
-  let canceledExecutions: SandboxExecution[];
+  let canceledExecutionIds: SandboxExecutionId[];
+  let logLocator: string;
 
   if (target instanceof SandboxExecution) {
     target.cts.cancel();
-    canceledExecutions = [target];
+    canceledExecutionIds = [target.id];
+    logLocator = target.id;
   } else {
-    const macroId = getMacroId(resolveUri(target));
-    canceledExecutions = sandboxManager.cancel(macroId);
+    const resolvedUri = resolveUri(target);
+    logLocator = formatDisplayUri(resolvedUri);
+
+    const uri = getMacroUriFromStartupMacroUri(resolvedUri);
+    canceledExecutionIds = sandboxManager.cancel(uri).map(({ id }) => id);
   }
 
-  log.info('Cancellation requested', ...canceledExecutions.map(({ id }) => id));
+  if (canceledExecutionIds.length === 0) {
+    log.info('No executions to cancel', logLocator);
+  } else {
+    log.info('Cancellation requested', ...canceledExecutionIds);
+  }
 }
